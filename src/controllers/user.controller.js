df48b626bf6f0e5163f2fa2f877c2c30c5184735
Hritesh-Kumar <2101628@kiit.ac.in>
2024-11-01 18:57:47 +0530

    Add refresh accesstoken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadInCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 const registerUser = asyncHandler(async (req, res) => {
   //------------
@@ -247,4 +248,48 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "user has been logged Out"));
 });
 
-export { registerUser, loginUser, logoutUser };
+//=======================================================
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "incoming refresh token not found");
+  }
+
+  const decodedToken = jwt.verify(incomingRefreshToken, REFRESH_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken?._id);
+
+  if (!user) {
+    throw new ApiError(401, "user khoj ne me dikat hai");
+  }
+
+  if (incomingRefreshToken !== user?.refreshToken) {
+    throw new ApiError(401, "incoming refresh token and user.token mismatch");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  const { refreshToken, accessToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  return res
+    .status("200")
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        "200",
+        { accessToken, refreshToken },
+        "refreshToken generated"
+      )
+    );
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
